1358c7d9e5a7e546cdbf2516aa0727cab17b352d
10morokoshi <laughmaker0421@gmail.com>
2022-04-01 01:23:33 +0900

    ルーティングの設定

--- routes/web.php
+++ routes/web.php
@@ -13,10 +13,9 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
-
 Auth::routes();
 
-Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+Route::get('/home', 'HomeController@index')->name('home');
+
+Route::get('/timeline', 'TimelineController@showTimelinePage')->name('timeline');
+Route::post('/timeline', 'TimelineController@postTweet')->name('timeline');
